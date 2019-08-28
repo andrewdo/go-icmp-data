1dfcf1889c3d45d6eb75f184d95ab08280f229bc
Andrew Do <andrew.do@panasonic.aero>
2019-08-28 11:42:07 -0700

    add docker

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:latest
+
+RUN curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+
+RUN go get -d github.com/andrewdo/go-icmp-data/...
+WORKDIR $HOME/go/src/github.com/andrewdo/go-icmp-data
+RUN dep ensure
+RUN mkdir /app
+RUN go build -o /app/server ./server
+RUN go build -o /app/client ./client
+
+CMD ["./app/server"]
--- client/client.go
+++ client/client.go
@@ -4,11 +4,21 @@ import (
 	"github.com/andrewdo/go-icmp-data/transport"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
-	transport.Send(&net.IPAddr{IP: net.ParseIP("127.0.0.1")}, 15, []byte("FFFF"))
-	x, y := transport.Receive()
+	addr, err := net.LookupIP("server")
+	if err != nil {
+		panic(err)
+	}
 
-	log.Println(x, y)
+	for {
+		transport.Send(&net.IPAddr{IP: addr[0]}, 15, []byte("FFFF"))
+		x, y := transport.Receive()
+
+		log.Println(x, y)
+
+		time.Sleep(10 * time.Second)
+	}
 }
